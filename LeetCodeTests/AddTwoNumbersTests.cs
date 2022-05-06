c85a00550a5e792d3661271c88767fa7f9ac674c
Andrew Youngho Chaa <andrew.yh.chaa@gmail.com>
2022-05-06 21:02:06 +0100

    wip

--- LeetCodeTests/AddTwoNumbersTests.cs
+++ LeetCodeTests/AddTwoNumbersTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace LeetCodeTests
@@ -8,13 +10,17 @@ namespace LeetCodeTests
         public void Should_add_two_linked_list()
         {
             // l1 = [2,4,3], l2 = [5,6,4]
-            var l1 = new ListNode(2, new ListNode(4, new ListNode(3, null)));
-            var l2 = new ListNode(5, new ListNode(6, new ListNode(4, null)));
+            // var l1 = new ListNode(2, new ListNode(4, new ListNode(3, null)));
+            var l1 = new ListNode(2);
+            AddNodes(l1, new []{4, 3});
+            
+            var l2 = new ListNode(5);
+            AddNodes(l2, new []{6, 4});
 
             var l3 = Add(l1, l2);
             
             Assert.Equal(7, l3.Val);
-            Assert.Equal(10, l3.Next.Val);
+            Assert.Equal(0, l3.Next.Val);
             Assert.Equal(7, l3.Next.Next.Val);
         }
 
@@ -34,8 +40,12 @@ namespace LeetCodeTests
         public void Should_add_two_list_with_different_length()
         {
             // l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
-            var l1 = new ListNode(0);
-            var l2 = new ListNode(0);
+
+            var l1 = new ListNode(9);
+            AddNodes(l1, new []{9,9,9,9,9,9});
+            
+            var l2 = new ListNode(9);
+            AddNodes(l2, new[]{9,9,9});
 
             var l3 = Add(l1, l2);
             
@@ -44,7 +54,7 @@ namespace LeetCodeTests
 
         private ListNode Add(ListNode l1, ListNode l2)
         {
-            var node = new ListNode(l1.Val + l2.Val);
+            var node = l1.Add(l2);
             SetNode(node, l1, l2);
 
             return node;
@@ -53,15 +63,18 @@ namespace LeetCodeTests
         private static void SetNode(ListNode node, ListNode l1, ListNode l2)
         {
             if (l1.Next == null && l2.Next == null) return;
-            
-            var nextNode = new ListNode(l1.Next.Val + l2.Next.Val);
-            node.Next = nextNode;
-            SetNode(nextNode, l1.Next, l2.Next);
+    
+            if (l1.Next != null)
+            node.Next = l1.Next.Add(l2.Next);
+            SetNode(node.Next, l1.Next, l2.Next);
         }
 
-        private static ListNode CreateNode(int[] values)
+        private static void AddNodes(ListNode node, IEnumerable<int> values)
         {
-            
+            if (!values.Any()) return;
+
+            node.Next = new ListNode(values.First());
+            AddNodes(node.Next, values.Skip(1));
         }
     }
 
@@ -74,6 +87,14 @@ namespace LeetCodeTests
         {
             Val = val;
             Next = next;
+        }
+
+        public ListNode Add(ListNode l)
+        {
+            var sum = Val + l.Val;
+            return new ListNode(sum >= 10
+                ? sum - 10
+                : sum);
         }
     }
 }
